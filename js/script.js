3266d9d3d0ffec6fab795333d84c66475beeb03b
georgiaboy02 <tbrewer1@mail.mvnu.edu>
2015-05-02 19:58:47 -0400

    fixed button

---
+++ js/script.js
@@ -0,0 +1,8 @@
+$(document).ready(function() {
+	$(".unimportant").click(function() {
+		console.log("you clicked unimportant")
+	});
+	$(".important").click(function() {
+		console.log("you clicked important")
+	});
+});
