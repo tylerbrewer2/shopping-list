d6be901e39d8a8b69cb3ce194e378149517d4b20
georgiaboy02 <tbrewer1@mail.mvnu.edu>
2015-05-02 20:57:51 -0400

    fixed unimportant to add box

--- js/script.js
+++ js/script.js
@@ -1,6 +1,7 @@
 $(document).ready(function() {
 	$(".unimportant").click(function() {
-		console.log("you clicked unimportant")
+		console.log("you clicked unimportant");
+		$(".items").prepend("<div class = 'box'><h4>Hello world</h4></div>");
 	});
 	$(".important").click(function() {
 		console.log("you clicked important")
