137942c3f5cd7d7eb9f2500703e2cc6b3b7e2b6b
georgiaboy02 <tbrewer1@mail.mvnu.edu>
2015-05-03 23:54:16 -0400

    added .val()

--- js/script.js
+++ js/script.js
@@ -1,11 +1,11 @@
 $(document).ready(function() {
 	$(".unimportant").click(function() {
-		console.log("you clicked unimportant");
-		$(".items").append("<div class = 'box'><h4>Goodbye world</h4></div>");
+		var inputText = $(".text-input").val();
+		$(".items").append("<div class = 'box'><h4>" + inputText + "</h4></div>");
 	});
 	$(".important").click(function() {
-		$(".items").prepend("<div class = 'box'><h4>Hello world</h4></div>");
-		console.log("you clicked important")
+		var inputText = $(".text-input").val();
+		$(".items").prepend("<div class = 'box'><h4>"+ inputText + "</h4></div>");
 	});
 	$(".items").click(function(e) {
 		$(e.target).remove();
